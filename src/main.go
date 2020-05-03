f78cc73a12356c10dc88bdc1e3af27269ab24827
Mattia Mari <mattiamari96@gmail.com>
2020-05-03 20:07:28 +0200

    Handle change availability

--- src/main.go
+++ src/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -50,15 +49,13 @@ type Weight struct {
 
 var db *sqlx.DB
 
-func handleRoot(w http.ResponseWriter, r *http.Request) {
+func RootHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Smart Dumpster Service"))
 }
 
-func handleDumpsters(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
+func DumpstersHandler(w http.ResponseWriter, r *http.Request) {
 	dumpsters := []Dumpster{}
 	err := db.Select(&dumpsters, "select * from dumpster")
 	if err != nil {
@@ -67,17 +64,66 @@ func handleDumpsters(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	json, _ := json.Marshal(dumpsters)
+	json, _ := json.Marshal(map[string]interface{}{"dumpsters": dumpsters})
 	w.WriteHeader(http.StatusOK)
 	w.Write(json)
 }
 
-func handleDumpster(w http.ResponseWriter, r *http.Request) {
+func DumpsterHandler(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
-	w.Header().Set("Content-Type", "application/json")
 
+	dumpster := Dumpster{}
+	err := db.Get(&dumpster, "select * from dumpster where id=$1", params["dumpsterId"])
+	if err != nil {
+		log.Print(err.Error())
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	json, _ := json.Marshal(map[string]interface{}{"dumpster": dumpster})
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf(`{"dumpster": {"id": "%s"}}`, params["dumpsterId"])))
+	w.Write(json)
+}
+
+func AvailabilityHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	if r.Method == http.MethodGet {
+		var availability bool
+		err := db.Get(&availability, "select available from dumpster where id=$1", params["dumpsterId"])
+		if err != nil {
+			log.Print(err.Error())
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		json, _ := json.Marshal(map[string]interface{}{"available": availability})
+		w.WriteHeader(http.StatusOK)
+		w.Write(json)
+	}
+
+	type Availability struct {
+		Available bool `json:"available"`
+	}
+
+	if r.Method == http.MethodPost {
+		availability := Availability{}
+		decoder := json.NewDecoder(r.Body)
+		decoder.Decode(&availability)
+
+		db.Exec("update dumpster set available=$1 where id=$2", availability.Available, params["dumpsterId"])
+
+		w.WriteHeader(http.StatusCreated)
+	}
+}
+
+func HeadersMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Cache-Control", "no-store")
+		next.ServeHTTP(w, r)
+	})
 }
 
 func main() {
@@ -92,12 +138,16 @@ func main() {
 	defer db.Close()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", handleRoot).Methods(http.MethodGet)
+	r.HandleFunc("/", RootHandler).Methods(http.MethodGet)
 
 	api := r.PathPrefix("/api/v1").Subrouter()
-	api.HandleFunc("/", handleRoot).Methods(http.MethodGet)
-	api.HandleFunc("/dumpsters", handleDumpsters).Methods(http.MethodGet)
-	api.HandleFunc("/dumpster/{dumpsterId}", handleDumpster).Methods(http.MethodGet)
+	api.Use(HeadersMiddleware)
+	api.HandleFunc("/", RootHandler).Methods(http.MethodGet)
+	api.HandleFunc("/dumpsters", DumpstersHandler).Methods(http.MethodGet)
+	api.HandleFunc("/dumpster/{dumpsterId}", DumpsterHandler).Methods(http.MethodGet)
+	api.HandleFunc("/dumpster/{dumpsterId}/availability", AvailabilityHandler).Methods(http.MethodGet)
+	api.HandleFunc("/dumpster/{dumpsterId}/availability", AvailabilityHandler).Methods(http.MethodPost).Headers("Content-Type", "application/json")
+	api.Use(mux.CORSMethodMiddleware(api))
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
