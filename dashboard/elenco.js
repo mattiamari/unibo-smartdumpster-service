c0a5b520f1b54fd9f9fcab2ef86ddd8daacf332e
Mattia Mari <mattiamari96@gmail.com>
2020-05-08 12:14:12 +0200

    Fix availability change

--- dashboard/elenco.js
+++ dashboard/elenco.js
@@ -1,19 +1,16 @@
-// url di mari nel quale si trovano i dati
-// http://mattiamari.me:8069/api/v1/dumpster/id-del-dumpster
-var apiUrl = 'http://mattiamari.me:8069/api/v1/dumpsters';
+var apiUrl = 'http://localhost:8080/api/v1';
 
 /*
 Chiamata AJAX all'url specificato e relativa funzione da chiamare per gestire il json.
 I dati sono ritornati parserizzati tramite $.parseJSON().
 */
-$.getJSON(apiUrl, function(data) {
+$.getJSON(apiUrl + "/dumpsters", function(data) {
 
     // Creo il bottone uguale per ogni dumpster.
     var btnDetails = '<td><button class="button">Abilita/Disabilita</button></td>';
 
-
     $.each(data.dumpsters, function(key, val) {
-        var tdId = `<td class="idDumpster"><a href="./statisticheBidone.html?id=${val.id}">${val.id}</a></td>`;
+        var tdId = `<td class="idDumpster"><a href="statisticheBidone.html?id=${val.id}">${val.id}</a></td>`;
         var tdName = '<td>' + val.name + '</td>';
         var tdStatus = '<td class="status">' + val.available + '</td>';
         var tdCountDumps = '<td>' + val.dumps_since_last_emptied + '</td>';
@@ -28,14 +25,14 @@ $.getJSON(apiUrl, function(data) {
 
     document.querySelector('.button').addEventListener("click", function() {
         status = this.parentNode.parentNode.querySelector('.status').innerHTML;
-        idDumpster = this.parentNode.parentNode.querySelector('.idDumpster').innerHTML;
+        idDumpster = this.parentNode.parentNode.querySelector('.idDumpster > a').innerHTML;
         status == 'true' ? status = 'false' : status = 'true';
         //this.parentNode.parentNode.querySelector('#status').innerHTML = status;
 
         // Invio il valore di status
         $.ajax({
             type: "POST",
-            url: "http://mattiamari.me:8069/api/v1/dumpster/" + idDumpster + "/availability",
+            url: apiUrl + "/dumpster/" + idDumpster + "/availability",
             contentType: "application/json",
             data: JSON.stringify({available: status})
         });
