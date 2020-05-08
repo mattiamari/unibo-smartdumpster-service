76b6ded7ceb8731e4d2ee38233c80e940d78654b
Mattia Mari <mattiamari96@gmail.com>
2020-05-08 11:39:53 +0200

    Add Dashboard files

---
+++ dashboard/dettagli.js
@@ -0,0 +1,143 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
+
+var apiUrl = "http://mattiamari.me:8069/api/v1/dumpster/" + id;
+
+
+var dataNumDumps = [];
+var dataWeight = [];
+var dataDumpsTime = [];
+var dataPoints = [];
+var arrayWeightLimit = [];
+var dumpsType = {};
+
+$.getJSON(apiUrl, function(data) {
+    weight_limit = data.dumpster["weight_limit"];
+    dump_history = data.dumpster["dump_history"];
+
+    weight_history = data.dumpster["weight_history"];
+
+
+    $.each(dump_history, function(key, val) {
+        dumpsType[val.dump_type] = (dumpsType[val.dump_type] || 0) + 1;
+    });
+    
+
+    $.each(weight_history, function(key, val) {
+        
+        dataNumDumps.push(val.dumps_since_last_emptied);
+        dataWeight.push(val.weight);
+
+        arrayWeightLimit.push(weight_limit);
+
+        var pos = val.created_at.lastIndexOf(".");
+
+        dataDumpsTime.push(val.created_at.slice(0, pos));
+
+        dataPoints.push({x: val.created_at.slice(0, pos), y: val.weight });
+    });
+
+
+
+    var ctx1 = document.getElementById('myChart1');
+
+    var myChart = new Chart(ctx1, {
+        type: 'line',
+  data: {
+        labels: dataNumDumps,
+        datasets: [{ 
+            data: dataWeight,
+            label: "Peso",
+            borderColor: "#3e95cd",
+            fill: false,
+        }, {
+            label: "Capienza massima",
+            borderColor: "rgb(0, 0, 0)",
+            data: arrayWeightLimit,
+            //borderDash: [0,50],
+            fill: false,  
+        }]
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Grafico dei depositi'
+        }
+    }
+});
+
+    var ctx2 = document.getElementById('myChart2');
+
+    var my = new Chart(ctx2, {
+        type: 'line',
+        data: {
+            labels: dataDumpsTime,
+
+            datasets: [{
+                label: 'Peso',
+                yAxisID: 'A',
+                data: dataWeight,
+                borderColor: "#ff6384",
+                fill: false
+            }, {
+                label: 'Numero deposito',
+                yAxisID: 'B',
+                borderColor: 'rgb(254, 217, 118)',
+                data: dataNumDumps,
+                fill: false
+            }]   
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Grafico temporale'
+            },
+            scales: {
+                yAxes: [{
+                   id: 'A',
+                   type: 'linear',
+                   position:'left' 
+                }, {
+                    id: 'B',
+                    type: 'linear',
+                    position: 'right'
+                }],
+                xAxes: [{
+                    type: 'time',
+                    time: {
+                        displayFormats: {
+                            millisecond: 'h:mm:ss.SSS'
+                        }
+                    }
+                }]
+            }
+        }
+    })
+
+    var dumpsTypeValue = []
+    var dumpsTypeOccurences = [];
+
+    for(var index in dumpsType) {
+        dumpsTypeOccurences.push(dumpsType[index]);
+        dumpsTypeValue.push(index)
+    }
+    
+    var ctx3 = document.getElementById('myChart3');
+
+    var a = new Chart(ctx3, {
+        type: 'pie',
+        data: {
+            datasets: [{
+                data: dumpsTypeOccurences,
+                backgroundColor: ["rgb(254, 217, 118)", "rgb(8, 88, 158)","rgb(150, 150, 150)"],
+            }],      
+            labels: dumpsTypeValue, 
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Distribuzione tipologia di rifiuti'
+            }
+        },
+    })
+});
---
+++ dashboard/elenco.js
@@ -0,0 +1,44 @@
+// url di mari nel quale si trovano i dati
+// http://mattiamari.me:8069/api/v1/dumpster/id-del-dumpster
+var apiUrl = 'http://mattiamari.me:8069/api/v1/dumpsters';
+
+/*
+Chiamata AJAX all'url specificato e relativa funzione da chiamare per gestire il json.
+I dati sono ritornati parserizzati tramite $.parseJSON().
+*/
+$.getJSON(apiUrl, function(data) {
+
+    // Creo il bottone uguale per ogni dumpster.
+    var btnDetails = '<td><button class="button">Abilita/Disabilita</button></td>';
+
+
+    $.each(data.dumpsters, function(key, val) {
+        var tdId = `<td class="idDumpster"><a href="./statisticheBidone.html?id=${val.id}">${val.id}</a></td>`;
+        var tdName = '<td>' + val.name + '</td>';
+        var tdStatus = '<td class="status">' + val.available + '</td>';
+        var tdCountDumps = '<td>' + val.dumps_since_last_emptied + '</td>';
+        var tdCurrentWeight = '<td>' + val.current_weight + '</td>';
+        var tdLimitWeight = '<td>' + val.weight_limit + '</td>';
+
+        // Riempio la tabella.
+        $('#dumpstersTable').append('<tr>' + tdId + tdName + tdCountDumps 
+            + tdCurrentWeight + tdLimitWeight + tdStatus + btnDetails + '</tr>');
+
+    });
+
+    document.querySelector('.button').addEventListener("click", function() {
+        status = this.parentNode.parentNode.querySelector('.status').innerHTML;
+        idDumpster = this.parentNode.parentNode.querySelector('.idDumpster').innerHTML;
+        status == 'true' ? status = 'false' : status = 'true';
+        //this.parentNode.parentNode.querySelector('#status').innerHTML = status;
+
+        // Invio il valore di status
+        $.ajax({
+            type: "POST",
+            url: "http://mattiamari.me:8069/api/v1/dumpster/" + idDumpster + "/availability",
+            contentType: "application/json",
+            data: JSON.stringify({available: status})
+        });
+    });
+});
+
